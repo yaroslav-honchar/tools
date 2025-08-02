33896140e07eabe6dab55e37dffcdfe9950ff0d6
Yaroslav Honchar <yaroslav.honchar@web-systems.com.ua>
2025-08-02 15:49:39 +0200

    create project

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const PORT = process.env.PORT || 3000;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
+}
+
+bootstrap()
+  .then(() => {
+    console.log(`Server is running on port ${PORT}`);
+  })
+  .catch((error) => {
+    console.error('Error starting the server:', error);
+  });
