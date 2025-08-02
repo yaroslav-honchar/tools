d01e87378cd1189ea109c3dd7726f56de05daaed
Yaroslav Honchar <yaroslav.honchar@web-systems.com.ua>
2025-08-02 21:10:08 +0200

    remove unused AppController and AppService from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { FontsModule } from './fonts/fonts.module';
 
 @Module({
   imports: [FontsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
